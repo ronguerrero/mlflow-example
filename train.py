51a40bfd93441ae075e83829a3e5a7b453013140
ronguerrero <43661927+ronguerrero@users.noreply.github.com>
2018-10-17 20:16:31 -0400

    added experiment id

--- train.py
+++ train.py
@@ -41,8 +41,9 @@ if __name__ == "__main__":
     train_y = train[["quality"]]
     test_y = test[["quality"]]
 
-    alpha = float(sys.argv[1]) if len(sys.argv) > 1 else 0.5
-    l1_ratio = float(sys.argv[2]) if len(sys.argv) > 2 else 0.5
+    exp_id = int(sys.argv[1])
+    alpha = float(sys.argv[2]) if len(sys.argv) > 1 else 0.5
+    l1_ratio = float(sys.argv[3]) if len(sys.argv) > 2 else 0.5
 
     with mlflow.start_run():
         lr = ElasticNet(alpha=alpha, l1_ratio=l1_ratio, random_state=42)
